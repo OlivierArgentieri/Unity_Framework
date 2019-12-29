9e040c125c02dd99b923d4a1d0350c3f27284ef5
OlivierArgentieri <olivierargentieri@gmail.com>
2019-12-29 15:34:27 +0100

    add tempalteManager Script

---
+++ Unity_Framework/Assets/Unity_Framework/Scripts/ManagerTemplate/ManagerTemplate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerTemplate<T> : MonoBehaviour where T : MonoBehaviour
+{
+    #region f/p
+    private static T instance = null;
+    public static T Instance => instance;
+
+    [SerializeField, Header("Keep ?")] private bool keep = false;
+    #endregion
+
+
+    #region unity methods
+
+    protected void Awake()
+    {
+        InitSingleton();
+    }
+
+    #endregion
+
+    #region custom methods
+
+    private void InitSingleton()
+    {
+        if (instance == null)
+            instance = this as T;
+
+        if (instance != this)
+            Destroy(gameObject);
+        
+        if(keep) DontDestroyOnLoad(gameObject);
+
+        name = "[MNG]";
+    }
+
+    #endregion
+}
