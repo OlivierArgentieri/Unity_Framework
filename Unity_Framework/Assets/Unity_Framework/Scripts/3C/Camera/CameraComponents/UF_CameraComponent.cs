ed81137759fd4f71f502474903a93c990d9a3ace
OlivierArgentieri <olivierargentieri@gmail.com>
2019-12-30 22:57:44 +0100

    start editor for cameraComponent and behaviour

--- Unity_Framework/Assets/Unity_Framework/Scripts/3C/Camera/CameraComponents/UF_CameraComponent.cs
+++ Unity_Framework/Assets/Unity_Framework/Scripts/3C/Camera/CameraComponents/UF_CameraComponent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 public class UF_CameraComponent: MonoBehaviour, IHandlerItem<int>
@@ -9,7 +10,11 @@ public class UF_CameraComponent: MonoBehaviour, IHandlerItem<int>
     
     [SerializeField, Header("Camera Settings")]
     private UF_CameraSetting cameraSettings = new UF_CameraSetting();
+
+    [SerializeField, Header("Camera Type")]
+    private CameraTypes cameraType = CameraTypes.Custom;
     
+    public CameraTypes CameraType => cameraType;
     #endregion
 
     #region unity mehods
@@ -26,6 +31,27 @@ public class UF_CameraComponent: MonoBehaviour, IHandlerItem<int>
     
     
     #region custom methods
+
+    public void InitBehaviour()
+    {
+        gameObject.GetComponents<UF_CameraBehaviour>().ToList().ForEach(DestroyImmediate);
+        UF_CameraBehaviour _behaviour = null;
+        switch (cameraType)
+        {
+            case CameraTypes.FPS:
+                _behaviour = gameObject.AddComponent<UF_CameraBehaviourFPS>();
+                break;
+            case CameraTypes.TPS:
+                break;
+            case CameraTypes.RTS:
+                break;
+            case CameraTypes.Custom:
+                break;
+        }
+        
+        _behaviour?.InitBehaviour(cameraSettings);
+    }
+    
     public void Register()
     {
         UF_CameraManager.Instance.Add(this);
@@ -37,4 +63,12 @@ public class UF_CameraComponent: MonoBehaviour, IHandlerItem<int>
         UF_CameraManager.Instance.Remove(this);
     }
     #endregion
+}
+
+public enum CameraTypes
+{
+    FPS,
+    TPS,
+    RTS,
+    Custom,
 }
--- Unity_Framework/Assets/Unity_Framework/Scripts/3C/Camera/Editor/CameraComponent/UF_CameraComponentEditor.cs
+++ Unity_Framework/Assets/Unity_Framework/Scripts/3C/Camera/Editor/CameraComponent/UF_CameraComponentEditor.cs
@@ -1,9 +1,47 @@
 
 using System;
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(UF_CameraComponent))]
 public class UF_CameraComponentEditor : Editor
 {
+    #region f/p
+
+    private CameraTypes previousCameraType;
+
+    #endregion
+    private static UF_CameraComponent eTarget = null;
+
+    #region unity methods
+
+    private void OnEnable()
+    {
+        eTarget = (UF_CameraComponent) target;
+    }
     
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+       
+        if (eTarget.CameraType.ToString() != previousCameraType.ToString())
+        {
+            previousCameraType = eTarget.CameraType;
+            InitCameraBehaviour();
+            Debug.Log(eTarget.CameraType.ToString() != previousCameraType.ToString());
+            Debug.Log(previousCameraType);
+        }
+    }
+    
+    #endregion
+
+
+    #region custom methods
+
+    void InitCameraBehaviour()
+    {
+        eTarget.InitBehaviour();
+    }
+
+    #endregion
 }
