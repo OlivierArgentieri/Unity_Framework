51357ddf16d1a013342bfb2fd79641736440323a
OlivierArgentieri <olivierargentieri@gmail.com>
2020-05-03 23:13:45 +0200

    need to remove class UF_Path, see todo
    #12

--- Unity_Framework/Assets/Unity_Framework/Scripts/Path/PathManager/Path/UF_Path.cs
+++ Unity_Framework/Assets/Unity_Framework/Scripts/Path/PathManager/Path/UF_Path.cs
@@ -8,19 +8,10 @@ namespace Unity_Framework.Scripts.Path.PathManager.Path
     [Serializable]
     public class UF_Path
     {
+        // todo remove : Because each path is a "PathModeSelector" object, so this class can be removed
+        
         #region f/p
-       
-        
         public UF_PathModeSelector PathMode = new UF_PathModeSelector();
-        
-        public bool IsMonoAgent = false;
-        public GameObject MonoAgent = null;
-        public List<GameObject> Agents = new List<GameObject>();
-
-        public float StartAtPercent = 0;
         #endregion
-        
-
-        
     }
 }
