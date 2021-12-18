2a7dc424ca3d22bf3693132e7e39ef7e2c674830
OlivierArgentieri <olivierargentieri@gmail.com>
2021-12-18 22:57:16 +0100

    lock open scenes tools in playmode

--- Unity_Framework/Assets/Unity_Framework/Scripts/Menu/Editor/UF_Scenes.cs
+++ Unity_Framework/Assets/Unity_Framework/Scripts/Menu/Editor/UF_Scenes.cs
@@ -11,19 +11,25 @@ namespace Unity_Framework.Scripts.Menu.Editor
         [MenuItem("UF/List Scenes")]
         static void Init()
         {
-            UF_Scenes window = (UF_Scenes) EditorWindow.GetWindow(typeof(UF_Scenes));
-            window.titleContent = new GUIContent("List of Scenes");
+            UF_Scenes _window = (UF_Scenes) EditorWindow.GetWindow(typeof(UF_Scenes));
+            _window.titleContent = new GUIContent("List of Scenes");
         }
         
         private void OnGUI() 
         {
+            if (Application.isPlaying)
+            {
+                 EditoolsBox.HelpBoxWarning("Only Available in editor mode");
+                 return;
+            }
+            
             var _scenes = Directory.GetFiles(Application.dataPath, "*.unity", SearchOption.AllDirectories);
             foreach (var _scene in _scenes)
             {
                 string _name = System.IO.Path.GetFileNameWithoutExtension(_scene);
-                EditoolsButton.Button(_name, Color.grey, () => { EditorSceneManager.OpenScene(_scene); });
+                
+                    EditoolsButton.Button(_name, Color.grey, () => { EditorSceneManager.OpenScene(_scene); });
             }
-
         }
         
     }
