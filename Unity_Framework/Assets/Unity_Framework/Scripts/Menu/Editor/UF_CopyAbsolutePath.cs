cb59232dcf76c031d0fd5ddf2af896e2c67d0d81
OlivierArgentieri <olivierargentieri@gmail.com>
2021-12-20 00:23:09 +0100

    add copy path method editor

--- Unity_Framework/Assets/Unity_Framework/Scripts/Menu/Editor/UF_CopyAbsolutePath.cs
+++ Unity_Framework/Assets/Unity_Framework/Scripts/Menu/Editor/UF_CopyAbsolutePath.cs
@@ -6,18 +6,26 @@ namespace Unity_Framework.Scripts.Menu.Editor
 {
     public static class UF_CopyAbsolutePath
     {
-        [MenuItem("Assets/Copy Absolute Path", priority = 1)]
+        [MenuItem("Assets/Copy Path/Copy Absolute Path", priority = 0)]
         static void CopyAbsolutePath()
         {
-            var _absolutePath = System.IO.Path.Combine(Application.dataPath, "../", AssetDatabase.GetAssetPath(Selection.activeObject));
-            GUIUtility.systemCopyBuffer = new Uri(_absolutePath).AbsolutePath;
+            var _relativePath = AssetDatabase.GetAssetPath(Selection.activeObject);
+            GUIUtility.systemCopyBuffer = System.IO.Path.GetFullPath(_relativePath); //new Uri(_absolutePath).AbsolutePath);
         }
         
-        [MenuItem("Assets/Copy Relative Path", priority = 0)]
-        static void CopyRelativePath()
+        [MenuItem("Assets/Copy Path/Copy Relative Path", priority = 0)]
+        static void CopyAssetPath()
         {
-            var _relativePath = AssetDatabase.GetAssetPath(Selection.activeObject);
-            GUIUtility.systemCopyBuffer = _relativePath;
+            var _assetPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+            GUIUtility.systemCopyBuffer = _assetPath;
+        }
+        
+        [MenuItem("Assets/Copy Path/Copy .meta file Path", priority = 0)]
+        static void CopyMetaFilePath()
+        {
+            var _assetPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var _metaPath = AssetDatabase.GetTextMetaFilePathFromAssetPath(_assetPath);
+            GUIUtility.systemCopyBuffer = System.IO.Path.GetFullPath(_metaPath);
         }
     }
 }
